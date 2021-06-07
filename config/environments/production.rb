793322af456f87033a259fafde9e061bc0354ad9
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-06-07 18:29:55 +0200

    change ipaddress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
 
 
-  config.asset_host = '165.232.141.176'
-  config.action_mailer.default_url_options = { host: '165.232.141.176', port: 3000 }
+  # config.asset_host = '165.232.141.176'
+  # config.action_mailer.default_url_options = { host: '165.232.141.176', port: 3000 }
 
 
   # Settings specified here will take precedence over those in config/application.rb.
