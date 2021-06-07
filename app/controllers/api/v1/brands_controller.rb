0b66348e10161b0d679b94eb49f95e4e96887f7f
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-06-07 23:35:38 +0200

    check error handling in brands

--- app/controllers/api/v1/brands_controller.rb
+++ app/controllers/api/v1/brands_controller.rb
@@ -5,20 +5,28 @@ class Api::V1::BrandsController < ApplicationController
     begin
       @brands = Brand.all
     rescue
-      render json: {
-        message: JSON.parse("No brands found!".to_json)
-      }, status: :not_found
-    else
-      render json: {
-        message: "List of brand(s)",
-        data: @brands.map{ |brand|
-          {
-            id: brand.id,
-            name: brand.name,
-            image: brand.image.url
-          }
-        }
-      }, status: :ok
+      respond_to do |format|
+        format.json do
+          render json: {
+            message: JSON.parse("No brands found!".to_json)
+          }.to_json, status: :not_found
+          end
+        end
+      else
+        respond_to do |format|
+          format.json do
+            render json: {
+              message: "List of brand(s)",
+              data: @brands.map{ |brand|
+                {
+                  id: brand.id,
+                  name: brand.name,
+                  image: brand.image.url
+                }
+              }
+            }.to_json, status: :ok
+          end
+        end
     end
   end
 end
