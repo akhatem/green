f35466eebe18dc660efa9e0933066e1f319a4bee
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-06-07 17:52:15 +0200

    change storage from aws to file

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -2,7 +2,8 @@ class ItemImageUploader < CarrierWave::Uploader::Base
     include CarrierWave::MiniMagick
   
     # storage :fog
-    storage :aws
+    # storage :aws
+    storage :file
   
     # def store_dir
     #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- app/uploaders/offer_image_uploader.rb
+++ app/uploaders/offer_image_uploader.rb
@@ -16,7 +16,8 @@ class OfferImageUploader < CarrierWave::Uploader::Base
     include CarrierWave::MiniMagick
   
     # storage :fog
-    storage :aws
+    # storage :aws
+    storage :file
   
     # def store_dir
     #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
