4104947cd6ba926fa16149a067d96d2c8a8700df
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-06-07 23:13:03 +0200

    Fix Api::V1::brands

--- app/controllers/api/v1/brands_controller.rb
+++ app/controllers/api/v1/brands_controller.rb
@@ -1,23 +1,23 @@
 class Api::V1::BrandsController < ApplicationController
   
   def index
-    @brands = Brand.all
-    if @brands.any?
-    render json: {
-      message: "List of brand(s)",
-      data: @brands.map{ |brand|
-        {
-          id: brand.id,
-          name: brand.name,
-          image: brand.image.url
-        }
-      }
-    }, status: :ok
+    begin
+      @brands = Brand.all
+    rescue
+      render json: {
+        message: JSON.parse("No brands found!".to_json)
+      }, status: :not_found
     else
       render json: {
-        message: "Error!",
-        error: JSON.parse("No Category found in brand #{brand.name}!".to_json)
-      }, status: :not_found
+        message: "List of brand(s)",
+        data: @brands.map{ |brand|
+          {
+            id: brand.id,
+            name: brand.name,
+            image: brand.image.url
+          }
+        }
+      }, status: :ok
     end
   end
 end
