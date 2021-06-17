eac89824a8c420e773aa6aa0180eae6092bf6690
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-06-17 10:30:41 +0200

    fix barcode v2

--- app/uploaders/customer_barcode_uploader.rb
+++ app/uploaders/customer_barcode_uploader.rb
@@ -3,9 +3,9 @@ class CustomerBarcodeUploader < CarrierWave::Uploader::Base
     # if Rails.env.development?
         storage :file
 
-        # def store_dir
-        #     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-        # end
+        def store_dir
+            "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+        end
 
         def cache_dir
         "tmp/uploads/cache/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
