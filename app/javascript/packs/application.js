9eb8db87c4caf9f1e04f0a401d612885f4ee564d
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-06-23 02:30:41 +0200

    fix brands edit page css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,17 +7,20 @@ import "channels"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 
+
+import "bootstrap";
+require("bootstrap")
+import "popper.js";
+require("popper.js")
+import "jquery";
+require("jquery")
+
+import "../stylesheets/application";
+import "@fortawesome/fontawesome-free/css/all"
+
 import "../js/custom"
 import "../js/footer"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
-require("bootstrap")
-import "popper.js";
-import "jquery";
-
-import "../stylesheets/application";
-import "@fortawesome/fontawesome-free/css/all"
