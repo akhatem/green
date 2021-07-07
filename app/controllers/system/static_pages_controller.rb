3f828e58a24c8b61936d9a095077127827bb9e89
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-07-07 12:31:02 +0200

    fix v1 barcode_search page

--- app/controllers/system/static_pages_controller.rb
+++ app/controllers/system/static_pages_controller.rb
@@ -8,14 +8,18 @@ class System::StaticPagesController < System::SystemApplicationController
 
   def barcode_search
     @customer = Customer.find_by(decoded_barcode: params[:decoded_barcode])
-    respond_to do |format|
-      if @customer
-        format.html {redirect_to system_customer_info_path(@customer.decoded_barcode), notice: "Customer #{@customer.name} found." }
-        format.json { render :customer_info, status: :ok }
-      else
-        format.html { render :barcode_search, alert: "Customer not found!", status: :not_found }
-        format.json { render json: @customer.errors, status: :not_found }
+    if params[:decoded_barcode]
+      respond_to do |format|
+        if @customer
+          format.html {redirect_to system_customer_info_path(@customer.decoded_barcode), notice: "Customer #{@customer.name} found." }
+          format.json { render :customer_info, status: :ok }
+        else
+          format.html { render :barcode_search, alert: "Customer not found!", status: :not_found }
+          format.json { render json: @customer.errors, status: :not_found }
+        end
       end
+    else
+      render :barcode_search
     end
   end
   
