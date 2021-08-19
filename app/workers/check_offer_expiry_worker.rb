fe640bf674f1b45893baee95f3afeb077a5741c6
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-08-19 16:04:04 +0200

    change check offer expiry from  greater than to less than

--- app/workers/check_offer_expiry_worker.rb
+++ app/workers/check_offer_expiry_worker.rb
@@ -3,7 +3,7 @@ class CheckOfferExpiryWorker
     
     def perform
       puts "Checking for expired offers..."
-      Offer.all.where("end_at > ?",  DateTime.now).where(state: 1).each do |offer|
+      Offer.all.where("end_at < ?",  DateTime.now).where(state: 1).each do |offer|
           offer.update(state: 0)
           puts "-= Offer #{offer.id} =- state changed to #{offer.state}!"
       end
