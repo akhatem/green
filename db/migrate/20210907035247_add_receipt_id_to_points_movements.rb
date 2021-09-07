964ab0a863fb9044e1d0ce24235833a209345887
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-09-07 07:26:20 +0200

    remove null: false in addReceiptIdToPointsMovments

--- db/migrate/20210907035247_add_receipt_id_to_points_movements.rb
+++ db/migrate/20210907035247_add_receipt_id_to_points_movements.rb
@@ -1,5 +1,5 @@
 class AddReceiptIdToPointsMovements < ActiveRecord::Migration[6.1]
   def change
-    add_reference :points_movements, :receipt, null: false, foreign_key: true
+    add_reference :points_movements, :receipt, foreign_key: true
   end
 end
