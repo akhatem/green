cf0884c96165b44ead240eebb7e19f72e2fc6d1f
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-11-14 02:16:26 +0200

    use old way to get daily points movements in points movements controller

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,20 +1,34 @@
 class UserPolicy < ApplicationPolicy
 
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
   def new_user?
-    !user.roleKey.eql?("cashier")
+    !@user.roleKey.eql?("cashier")
   end
 
   def create_user?
-    !user.roleKey.eql?("cashier")
+    !@user.roleKey.eql?("cashier")
   end
 
   def destroy_user?
-    !user.roleKey.eql?("cashier")
+    !@user.roleKey.eql?("cashier")
   end
   class Scope < Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+    
     def resolve
-      unless user.roleKey.eql?("cashier")
-        scope.all
+      unless @user.roleKey.eql?("cashier")
+        @scope.all
       else
         raise Pundit::NotAuthorizedError, 'not allowed to view this action'
       end
