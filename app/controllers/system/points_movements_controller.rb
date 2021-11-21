3e139729bc781cab9204bfd3277e07ce85c2b12a
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-11-21 11:03:02 +0200

    use shift on @daily_points_movements array

--- app/controllers/system/points_movements_controller.rb
+++ app/controllers/system/points_movements_controller.rb
@@ -60,10 +60,9 @@ class System::PointsMovementsController < System::SystemApplicationController
     daily_points_movements = []
     @branches.each do |branch|
       daily_points_movements |= PointsMovement.where(branch_id: branch.id)
-      # .order(branch_id: :asc)
-      .group(:branch_id)
       .group("DATE(date_time)")
       .order("DATE(date_time) ASC")
+      .group(:branch_id)
       .pluck(:branch_id, "DATE(date_time)", "SUM(earned)" , "SUM(redeemed)" , "SUM(total)")
       .map { |branch_id, date_time, earned, redeemed, total| 
         { 
