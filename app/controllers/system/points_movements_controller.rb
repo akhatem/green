8713a67993a420a105799d1ebdacbf4c9c4b0ca0
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-11-22 22:54:09 +0200

    first push to heroku

--- app/controllers/system/points_movements_controller.rb
+++ app/controllers/system/points_movements_controller.rb
@@ -59,21 +59,12 @@ class System::PointsMovementsController < System::SystemApplicationController
 
     daily_points_movements = []
     @branches.each do |branch|
-      daily_points_movements |= PointsMovement.where(branch_id: branch.id)
+      daily_points_movements.push(PointsMovement.where(branch_id: branch.id)
       .group(:branch_id)
       .order(branch_id: :asc)
-      .group("DATE(date_time)")
       .order("DATE(date_time) ASC")
-      .pluck(:branch_id, "DATE(date_time)", "SUM(earned)" , "SUM(redeemed)" , "SUM(total)")
-      # .map { |branch_id, date_time, earned, redeemed, total| 
-      #   { 
-      #     branch_id: branch_id,
-      #     date_time: date_time,
-      #     earned: earned,
-      #     redeemed: redeemed,
-      #     total: total 
-      #   }
-      # }
+      .group("DATE(date_time)")
+      .pluck(:branch_id, "DATE(date_time)", "SUM(earned)" , "SUM(redeemed)" , "SUM(total)"))
     end
     @pagy_a, @daily_points_movements = pagy_array(daily_points_movements)
 
