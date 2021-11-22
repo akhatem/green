55ba7ad44d1d96f961ffd17a1134fba5d35dab7d
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2021-11-22 23:19:55 +0200

    edit production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
 
-  config.asset_host = 'http://green.mmgunited.com'
+  # config.asset_host = 'http://green.mmgunited.com'
   # config.asset_host = 'http://localhost'
   
   # config.action_mailer.default_url_options = { host: 'http://green.mmgunited.com', port: 3000 }
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
